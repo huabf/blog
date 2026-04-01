dacad1b367a24fb9bd574c6c0f5f8b28c6dcce16
huabf <2940716783@qq.com>
2026-04-01 19:47:52 +0800

    fix(widget): show image storage as GitHub

--- app/components/widget/BlogTech.vue
+++ app/components/widget/BlogTech.vue
@@ -35,7 +35,7 @@ const [pm, pmVersion] = packageManager.split('@') as [string, string]
 			if (!avatar)
 				return '未知'
 			if (avatar.startsWith('/'))
-				return [h(Icon, { name: 'ph:folder' }), ' 仓库 public']
+				return [h(Icon, { name: 'ph:folder' }), ' GitHub']
 			try {
 				const u = new URL(avatar)
 				return [h(Icon, { name: hostIcon(u.hostname), alt: '' }), ` ${u.hostname}`]
